14c4cab24f5b60b6a5cff33da65ff5daa9d13692
Matheus Messias <matheus.jf@live.com>
2024-08-29 10:46:17 -0300

    refactor: update query logic to include results based on query params correctly

--- src/routes/list.ts
+++ src/routes/list.ts
@@ -28,13 +28,32 @@ router.get(
       })
     }
 
-    const measures = await db.query.measures.findMany({
-      where({ customer_code }, { eq }) {
-        return eq(customer_code, req.params.customer_code)
-      },
-    })
+    let measures
 
-    if (!measures) {
+    if (result.data.measure_type !== undefined) {
+      measures = await db.query.measures.findMany({
+        columns: {
+          customer_code: false,
+        },
+        where({ customer_code, measure_type }, { eq, and }) {
+          return and(
+            eq(customer_code, result.data.customer_code),
+            eq(measure_type, result.data.measure_type as 'WATER' | 'GAS'),
+          )
+        },
+      })
+    } else {
+      measures = await db.query.measures.findMany({
+        columns: {
+          customer_code: false,
+        },
+        where({ customer_code }, { eq }) {
+          return eq(customer_code, req.params.customer_code)
+        },
+      })
+    }
+
+    if (measures.length === 0) {
       return res.status(404).json({
         error_code: 'MEASURES_NOT_FOUND',
         error_description: 'Nenhuma leitura encontrada',
